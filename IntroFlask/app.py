b5131f6d0ecec06f734e7e55a3821e7cf2a08ec7
depth81 <70823176+yontaru@users.noreply.github.com>
2021-09-25 14:32:15 -0500

    Actualizacion

--- IntroFlask/app.py
+++ IntroFlask/app.py
@@ -1,7 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_mysqldb import MySQL
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'bdflask'
+
+conexion = MySQL(app)
+
 # endpoint o rutas
+
+@app.route('/cars')
+def list_cars():
+        data = {}
+        try:
+                cursor = conexion.connection.cursor()
+                sql = "SELECT id, marca, modelo, valor FROM car ORDER BY marca"
+                cursor.execute(sql)
+                companies = cursor.fetchall()
+                # print(companies)
+                data['mensaje'] = 'Exito'
+                data['cars'] = cars
+        except Exception as ex:    
+                data['mensaje'] = 'Error ...'
+        return jsonify(data)
+
+
 
 @app.route('/')
 def index():
@@ -21,5 +46,12 @@ def index():
 def login():
     return render_template('login.html')
 
+def not_found(error):
+    # return render_template('not_found.html'),404
+    return redirect(url_for('index'))
+
+app.register_error_handler(404, not_found)
+
 if __name__ == "__main__":
     app.run(debug=True,port=3200)
+
